6cc1d1303ffba3457e4be3f2e7bc0c9e7d567ad1
Ahmad-Yu2up-Ar-Raf <yusufzolldyck@gmail.com>
2026-09-18 11:28:04 +0700

    fix: adjust padding in AppShell component for better content spacing

--- frontend/src/components/ui/core/layout/dashboard/app-shell.tsx
+++ frontend/src/components/ui/core/layout/dashboard/app-shell.tsx
@@ -22,7 +22,7 @@ export function AppShell() {
           <AppSidebarHeader />
 
           {/* Wrapper Konten Outlet - Tambahkan padding p-4 agar konten tidak terlalu mepet layar */}
-          <div className="flex min-w-0 flex-1 flex-col gap-4 overflow-y-auto p-4 md:p-6">
+          <div className="flex min-w-0 flex-1 flex-col gap-4 overflow-y-auto p-3 md:p-0">
             <Outlet />
           </div>
         </SidebarInset>
